2fef3f0aad072f3ec185c336d3779b92aaf5e438
Yosep Alfatah <42576520+yoscruizer@users.noreply.github.com>
2021-01-19 22:25:46 +0700

    baru paham letak dan fungsi
    baru paham letak dan fungsi

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="Hallo My name is Yosep"/>
   </div>
 </template>
 
